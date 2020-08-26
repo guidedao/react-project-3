03ee81bea369a4dcaa7192db5f7ff4b8202553e7
Moscow Coding School <yo@moscoding.ru>
2020-08-27 02:18:51 +0300

    another check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 
 function App() {
   return (
@@ -11,4 +11,4 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
