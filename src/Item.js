c9d610d3606ab60cea2660147e7ab9586af89ee9
Igor Kasyan <76487504+ohhhucc@users.noreply.github.com>
2021-06-15 13:56:22 +0300

    initial commit

--- src/Item.js
+++ src/Item.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
 
-export default function Item() {
+export default function Item(props) {
+  let [quantity, setQuantity] = useState(0);
+
+  let handlePlusClick = () => {
+    setQuantity(quantity + 1);
+  };
+
+  let handleMinusClick = () => {
+    setQuantity(quantity - 1);
+  };
+
   return (
     <div className="item">
-      <img src="Сюда идет картинка" />
+      <img src={props.items.image} />
       <div className="item-info">
-        <h2>Сюда идет название</h2>
-        <p>Сюда идет описание</p>
+        <h2>{props.items.name}</h2>
+        <p>{props.items.desc}</p>
       </div>
       <div className="item-quantity">
-        <button className="item-less">-</button>
-        <h3 className="item-count">0</h3>
-        <button className="item-more">+</button>
+        <button
+          className="item-less"
+          onClick={handleMinusClick}
+          disabled={quantity === 0}
+        >
+          -
+        </button>
+        <h3 className="item-count">{quantity > 0 && quantity}</h3>
+        <button className="item-more" onClick={handlePlusClick}>
+          +
+        </button>
       </div>
     </div>
   );
